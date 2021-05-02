eec4c4e56daed64077db96852f39384624c9af2e
Yuriy <kain8370@yandex.ru>
2021-05-02 12:32:14 +0300

    закончил header

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+
+import appHeader from './app-header.module.css';
+import HeaderItem from '../header-item/header-item';
+
+class AppHeader extends React.Component {
+  state = {
+    items: [
+      {
+        iconName: BurgerIcon,
+        text: 'Конструктор',
+        isActive: true
+      },
+      {
+        iconName: ListIcon,
+        text: 'Лента заказов',
+        isActive: false
+
+      },
+      {
+        iconName: Logo,
+        text: null,
+        isActive: false
+      },
+      {
+        iconName: ProfileIcon,
+        text: 'Личный кабинет',
+        isActive: false
+      }
+    ]
+  }
+  
+  render() {
+    const headerElements = this.state.items.map((item, index) => {
+      let i = '';
+      if (item.iconName === 'Logo') {
+        i = <Logo />;
+      } else if (item.isActive) {
+        i = <item.iconName type="primary" />;
+      } else {
+        i = <item.iconName type="secondary" />;
+      }
+      return <HeaderItem key={index + 1} icon={i} text={item.text} isActive={item.isActive} />
+    });
+    console.log(headerElements); 
+    return (
+      <header className={appHeader.header}>
+        <ul className={appHeader.container}>
+          {headerElements}
+        </ul>
+        
+      </header>
+    )};
+}
+
+export default AppHeader;
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import data from '../../utils/data.js';
+
+import AppHeader from '../app-header/app-header';
 
 import app from './app.module.css';
 
 function App() {
   return (
     <div className={app.app}>
+      <AppHeader />
     </div>
   );
 }
